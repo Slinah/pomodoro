b975376e578e9186d2707446b1795e6f51f1a1bf
MENANTEAU Cédric <cedric.menanteau@epsi.fr>
2020-01-10 00:10:49 +0100

    Init

---
+++ PomodoroAPI/PomodoroAPI/Controllers/PomodoroController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PomodoroAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PomodoroController : ControllerBase
+    {
+        private readonly ILogger<PomodoroController> _logger;
+
+        public PomodoroController(ILogger<PomodoroController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pomodoro> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Pomodoro
+            {
+                Date = DateTime.Now.AddDays(index),
+                Tag = "Bla",
+                Timer = 800
+            }).ToArray();
+        }
+
+        [Route("/pomodoro/{t}")]
+        [HttpGet]
+        public IEnumerable<Pomodoro> Get(string t)
+        {
+            return Enumerable.Range(1, 5).Select(index => new Pomodoro
+            {
+                Date = DateTime.Now.AddDays(index),
+                Tag = t,
+                Timer = 800
+            }).ToArray();
+        }
+    }
+
+
+}
+
---
+++ PomodoroAPI/PomodoroAPI/Pomodoro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PomodoroAPI
+{
+    public class Pomodoro
+    {
+        public string Tag { get; set; }
+        public int Timer { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
